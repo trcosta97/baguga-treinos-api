5b4e7292991d2dad957654f0d66d37713a25875d
Thiago Ribeiro <trcosta97@hotmail.com>
2025-01-08 10:21:57 -0300

    first commit

---
+++ src/main/java/com/thiago/bagugatreino/BagugatreinoApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.bagugatreino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagugatreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagugatreinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/bagugatreino/BagugatreinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.bagugatreino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagugatreinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
